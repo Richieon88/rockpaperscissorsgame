8aac54cc0c2819e80b72786c5b22bd3c14e48f3d
Richieon88 <>
2023-09-09 22:31:32 +0000

    Add event listener for paper button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log('hello');
+const rockUser = document.getElementById('rock-user');
+const paperUser = document.getElementById('paper-user');
+const scissorsUser = document.getElementById('scissors-user');
+const rockComputer = document.getElementById('rock-com');
+const paperComputer = document.getElementById('paper-com');
+const scissorsComputer = document.getElementById('scissors-com');
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+
+paperButton.addEventListener('click', () => {
+    console.log('hi');
+});
